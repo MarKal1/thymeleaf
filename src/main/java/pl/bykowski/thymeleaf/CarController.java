a1302bad22daec1eb868028255e3bef133e40e7b
Martin Kalinowski <74590534+MarKal1@users.noreply.github.com>
2021-01-06 23:43:24 +0100

    newest change

--- src/main/java/pl/bykowski/thymeleaf/CarController.java
+++ src/main/java/pl/bykowski/thymeleaf/CarController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
 
     @GetMapping("/car")
     public String getCar (Model model) {
-       Car car1 = new Car ("Volvo", "i8");
-       model.addAttribute("car", car1);
+
+       Car car1 = new Car ("Ford", "Focus");
+       Car car2 = new Car ("Toyota", "Avensis");
+       Car car3 = new Car ("Suzuki", "Swift");
+       Car car4 = new Car ("Dacia", "Duster");
+       List<Car> cars = Arrays.asList(car1,car2,car3,car4);
+       model.addAttribute("cars", cars);
         return "index";
     }
 }
