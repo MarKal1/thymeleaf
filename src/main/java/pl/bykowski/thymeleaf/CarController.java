8095caaa8ea3814a01540a972a06755dc1abbd58
Martin Kalinowski <74590534+MarKal1@users.noreply.github.com>
2021-01-07 23:54:10 +0100

    added form that adds to list

--- src/main/java/pl/bykowski/thymeleaf/CarController.java
+++ src/main/java/pl/bykowski/thymeleaf/CarController.java
@@ -3,6 +3,8 @@ package pl.bykowski.thymeleaf;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,16 +13,27 @@ import java.util.List;
 @Controller
 public class CarController {
 
+    private List<Car> cars;
+
+    public CarController() {
+    Car car1 = new Car("Ford", "Focus");
+    Car car2 = new Car("Toyota", "Avensis");
+    Car car3 = new Car("Suzuki", "Swift");
+    Car car4 = new Car("Dacia", "Duster");
+    List<Car> cars = Arrays.asList(car1, car2, car3, car4);
+}
 
     @GetMapping("/car")
     public String getCar (Model model) {
-
-       Car car1 = new Car ("Ford", "Focus");
-       Car car2 = new Car ("Toyota", "Avensis");
-       Car car3 = new Car ("Suzuki", "Swift");
-       Car car4 = new Car ("Dacia", "Duster");
-       List<Car> cars = Arrays.asList(car1, car2, car3, car4);
        model.addAttribute("cars", cars);
-        return "index";
+        model.addAttribute("newCar", new Car ());
+         return "index";
     }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car){
+        System.out.println(car);
+        return"redirect:/car";
+    }
+
 }
